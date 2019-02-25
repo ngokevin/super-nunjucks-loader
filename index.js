903381390ce2f9c18a8f90bde4ca53b0876334ce
Kevin Ngo <me@ngokevin.com>
2019-02-25 13:35:58 -0800

    mark includes/include root in case future loaders want to mark dep

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const Nunjucks = require('nunjucks');
 const getOptions = require('loader-utils').getOptions;
 
+const includeRe = /{%\s*include\s*['"](.*?)['"]\s+%}/g
+
 module.exports = function (source) {
   const options = getOptions(this);
+  options.options = options.options || {};
+  options.options.noCache = true;
   const nunjucks = Nunjucks.configure(options.path, options.options);
 
+  // Store includes in case future loaders want to support HMR and get Webpack to watch
+  // for changes using require.
+  let match;
+  const includes = [];
+  while (match = includeRe.exec(source)) {
+    includes.push(match[1]);
+  }
+  let includesMarker = '';
+  if (includes.length) {
+    includesMarker = `\n<!-- <includes>${includes.join(',')}<end-includes> -->`;
+  }
+  const includeRootMarker = `\n<!-- <include-root>${options.path}<end-include-root> -->`;
+
+  // Add globals.
   for (let global in options.globals) {
     nunjucks.addGlobal(global, options.globals[global]);
   }
 
-  return nunjucks.renderString(source, options.context);
+  return nunjucks.renderString(source, options.context) + includesMarker + includeRootMarker;
 };
